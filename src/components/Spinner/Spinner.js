96370982e88fe7092e05987de85f082fe58caa47
Shoozle <sean@seanmurtagh.uk>
2021-03-25 22:51:50 +0000

    Msg when loading for Heroku db slowness

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './Spinner.module.css';
 
 const spinner = () => (
-    <div className={classes.spinner}>Loading...</div>
+    <div>
+        <p className={classes.warning}>Heroku DB can be slow when inactive for a while</p>
+        <div className={classes.spinner}>Loading...</div>
+    </div>
+    
 )
 
 export default spinner;
